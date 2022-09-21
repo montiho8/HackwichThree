5471132a0473917fe25f0a2255b87826f9cb46b2
montiho8 <montiho8@hawaii.edu>
2022-09-20 23:43:22 -1000

    Completed Part 4 problem sets

--- HackwichThree/ViewController.swift
+++ HackwichThree/ViewController.swift
@@ -19,18 +19,13 @@ class ViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    @IBOutlet weak var firstLabel: UILabel!
+    
+    @IBOutlet weak var secondLabel: UILabel!
+    
     @IBAction func changeColorButtonPressed(_ sender: Any) {
         if firstString == "The background color will turn red"
+        
         {
           //since conditional statement evaluates to false, it will skip the code in the if block
             self.view.backgroundColor = UIColor.red
@@ -39,6 +34,64 @@ class ViewController: UIViewController {
          
             self.view.backgroundColor = UIColor.blue
         }
-}
+        
+        firstLabel.text = "Red"
+        
+        secondLabel.text = "Blue"
+       
+        
+        if firstLabel.text == "Green"
+        {
+            print("I like this color")
+        }
+    
+        else{
+            firstLabel.text = "Red"
+            print("This color is nicer")
+        }
+    
+        
+        if secondLabel.text == "Blue"
+        {
+            print("This is the best color")
+        }
+    
+        else{
+            secondLabel.text = "Purple"
+            print("This is my favorite color")
+        }
+    
+    
+    }
+
+    var thirdString = "The background color will turn green"
+    var fourthString = "The background color will turn yellow"
+    
+    @IBOutlet weak var myThirdLabel: UILabel!
+    
+    @IBAction func magicButtonPressed(_ sender: Any) {
+
+        
+        if thirdString == "The background color will turn green"
+        {
+        self.view.backgroundColor = UIColor.green
+    }else{
+        self.view.backgroundColor = UIColor.yellow
+    }
+    
+        myThirdLabel.text = "Green"
+        
+        if myThirdLabel.text == "Green"
+        {
+            print("I completed both problem sets")
+        }
+        else{
+            myThirdLabel.text = "Blue"
+            print("Try Again")
+        }
+    
+    
+    }
+    
 
 }
